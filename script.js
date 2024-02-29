32a3a08bbf75e4bcd4c392d4ffe362ac53492e59
Tino Alakari <tino.alakari@gmail.com>
2024-02-29 13:28:25 -0700

    Finished project page, updated scripts, added images to resources

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ function displayText() {
         text.style.display = "none"
     }
   }
+
+  function displayImg1() {
+    const image = document.getElementById("image1");
+    if (image.style.display === "none") {
+        image.style.display = "block";
+    } else {
+        image.style.display = "none"
+    }
+  }
+
+  function displayImg2() {
+    const image = document.getElementById("image2");
+    if (image.style.display === "none") {
+        image.style.display = "block";
+    } else {
+        image.style.display = "none"
+    }
+  }
+
+  function displayImg3() {
+    const image = document.getElementById("image3");
+    if (image.style.display === "none") {
+        image.style.display = "block";
+    } else {
+        image.style.display = "none"
+    }
+  }
+
